d8953e770bdeacac43d919775f85d8aac3a850a7
Alex Liew <liewtwoo42@gmail.com>
2023-07-07 14:14:51 -0400

    finish us-states-game

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-
-answer_state = screen.textinput(title="Guess the State", prompt="What's another states name?").title()
-
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
-if answer_state in all_states: 
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states guessed", prompt="What's another states name?").title()
+
+    if answer_state in all_states: 
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 
-turtle.mainloop()
+
+screen.exitonclick()
